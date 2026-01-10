d5cda69451d7cfc5acf2cda65094a0c11468e6f1
unknown <fatemeh.khorshidvand1@gmail.com>
2026-01-10 09:36:42 +0330

    test::

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,12 +4,12 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello() {
     console.log("/////");
+    console.log("//////")
     return {
       message: 'کد ورود من',
       statusCode: 200,
       data: 'کد ورود منقضی شده است'
     }
   }
-
 
 }
